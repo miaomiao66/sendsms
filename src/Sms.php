4b307e8ca146dc0ed4678fae8cde71b855c81820
miao <miao@miaoqi-MacBook-Pro.local>
2017-09-12 11:04:06 +0800

    update

--- src/Sms.php
+++ src/Sms.php
@@ -2,115 +2,31 @@
 
 namespace Miaoqi\SendSms;
 
-class Sms 
+class Sms
 {
-	/**
-     * 请求地址
-     *
-     * @var string
-     */
-	private $sendUrl;
-
-	/**
-     * 账户
-     *
-     * @var string
-     */
-	private $account;
-
-	/**
-     * 密码
-     *
-     * @var string
-     */
-	private $password;
-
-    public function __construct()
-    {
-    	$curConfigs = file_exists(config_path('sendsms.php')) ? require(config_path('sendsms.php')) : require(__DIR__ . '/config/default.php');
-    	$this->sendUrl = $curConfigs['api_send_url'];
-    	$this->account = $curConfigs['api_account'];
-    	$this->password = $curConfigs['api_password'];
-    }
-
-	/**
-	 * 发送短信
-	 *
-	 * @param string $mobile 	  手机号码
-	 * @param string $msg 		  短信内容
-	 * @param string $needstatus  是否需要状态报告
-	 */
-	public function sendSMS($mobile, $msg, $needstatus = 1) 
-	{	
-		$postArr = array(
-				'un' => $this->account,
-				'pw' => $this->password,
-		        'msg' => $msg,
-		        'phone' => $mobile,
-		        'rd' => $needstatus
-			);
-		$result = $this->curlPost($this->sendUrl, $postArr);
-		$result = preg_split("/[,\r\n]/", $result);
-		return $this->results()[$result[1]];
-	}
-
-	/**
-	 * 通过CURL发送HTTP请求
-	 * 
-	 * @param  string  $url  
-	 * @param  array   $postFields 
-	 * @return mixed
-	 */
-	private function curlPost($url, $postFields)
+	public static function createObject($agent)
 	{
-		$postFields = http_build_query($postFields);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;
-	}
+		switch ($agent) {
+			case 'RongLian':
+				if (include_once("./RongLian.php")) {
+					$object = new RongLian();
+				}
+				break;
+			
+			case 'ChuangLan':
+				if (include_once("./ChuangLan.php")) {
+					$object = new ChuangLan();
+				}
+				break;
 
-	/**
-	 * 创蓝接口返回参数
-	 * 
-	 * @return array
-	 */
-	private function results()
-	{
-		return array(
-			'0' =>'发送成功',
-		    '101'=>'无此用户',
-		    '102'=>'密码错',
-		    '103'=>'提交过快',
-		    '104'=>'系统忙',
-		    '105'=>'敏感短信',
-		    '106'=>'消息长度错',
-		    '107'=>'错误的手机号码',
-		    '108'=>'手机号码个数错',
-		    '109'=>'无发送额度',
-		    '110'=>'不在发送时间内',
-		    '111'=>'超出该账户当月发送额度限制',
-		    '112'=>'无此产品',
-		    '113'=>'extno格式错',
-		    '115'=>'自动审核驳回',
-		    '116'=>'签名不合法，未带签名',
-		    '117'=>'IP地址认证错',
-		    '118'=>'用户没有相应的发送权限',
-		    '119'=>'用户已过期',
-		    '120'=>'内容不是白名单',
-		    '121'=>'必填参数。是否需要状态报告，取值true或false',
-		    '122'=>'5分钟内相同账号提交相同消息内容过多',
-		    '123'=>'发送类型错误(账号发送短信接口权限)',
-		    '124'=>'白模板匹配错误',
-		    '125'=>'驳回模板匹配错误'
-		);
+			default:
+				$object = '参数错误';
+				break;
+		}
+		return $object;
 	}
 }
+
 
 
 ?>
--- src/config/default.php
+++ src/config/default.php
@@ -1,8 +1,63 @@
 <?php
 
 return [
-    'api_send_url' => 'http://sms.253.com/msg/send',
-    'api_account' => 'your_account',
-    'api_password' => 'your_password'
+	'default' => 'RongLian',
+
+	'agents' => [
+
+      //   'ChuangLan' => [
+      //       'api_send_url' => 'http://sms.253.com/msg/send',
+    		// 'api_account' => 'your_account',
+    		// 'api_password' => 'your_password'
+      //   ],
+
+        'ChuangLan' => [
+            'api_send_url' => 'http://sms.253.com/msg/send',
+    		'api_account' => 'N8095389',
+    		'api_password' => 'Eu7LOKagPs557e'
+        ],
+
+   //      'RongLian' => [
+   //          //主帐号,对应官网开发者主账号下的 ACCOUNT SID
+			// 'api_account_sid' => 'your_account_sid',
+
+			// //主帐号令牌,对应官网开发者主账号下的 AUTH TOKEN
+			// 'api_auth_token' => 'your_auth_token',
+
+			// //应用Id,在官网应用列表中点击应用,对应应用详情中的APP ID,在开发调试的时候,可以使用官网自动为您分配的测试Demo的APP ID
+			// 'api_app_id' => 'your_app_id',
+
+			// //请求地址,沙盒环境（用于应用开发调试）：sandboxapp.cloopen.com,生产环境(用户应用上线使用):app.cloopen.com
+			// 'api_server_ip' => 'server_ip',
+
+			// //请求端口,生产环境和沙盒环境一致
+			// 'api_server_port' => 'server_port',
+
+			// //REST版本号，在官网文档REST介绍中获得。
+			// 'api_soft_version' => 'soft_version',
+   //      ],
+
+        'RongLian' => [
+            //主帐号,对应官网开发者主账号下的 ACCOUNT SID
+			'api_account_sid' => '8aaf07085e6037fd015e6065073e0043',
+
+			//主帐号令牌,对应官网开发者主账号下的 AUTH TOKEN
+			'api_auth_token' => '38cc1c8efa9c4aecbaaab9a533a2c9de',
+
+			//应用Id,在官网应用列表中点击应用,对应应用详情中的APP ID,在开发调试的时候,可以使用官网自动为您分配的测试Demo的APP ID
+			'api_app_id' => '8aaf07085e6037fd015e606508ba004a',
+
+			//请求地址,沙盒环境（用于应用开发调试）：sandboxapp.cloopen.com,生产环境(用户应用上线使用):app.cloopen.com
+			'api_server_ip' => 'sandboxapp.cloopen.com',
+
+			//请求端口,生产环境和沙盒环境一致
+			'api_server_port' => '8883',
+
+			//REST版本号，在官网文档REST介绍中获得。
+			'api_soft_version' => '2013-12-26',
+        ],
+
+    ],
+
 ];
 
